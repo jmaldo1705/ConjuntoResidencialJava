9ae0bb576d15e5b37d5576af6e0266e9ffeff137
Jonathan <13889809+jmaldo1705@users.noreply.github.com>
2024-06-26 15:13:16 -0500

    first commit

---
+++ src/main/java/com/conjuntoresidencialjava/ConjuntoResidencialJavaApplication.java
@@ -0,0 +1,13 @@
+package com.conjuntoresidencialjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConjuntoResidencialJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConjuntoResidencialJavaApplication.class, args);
+    }
+
+}
